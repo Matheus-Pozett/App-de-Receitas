dc6eb970ca4134b7b76635b9d5ee1f72faa315dd
Matheus Pozett <maa_souza11@hotmail.com>
2025-07-03 11:45:19 -0300

    feat: cria formulario da pagina de login e suas validações

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AppRoutes from './routes';
 
 function App() {
-  return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
-  );
+  return (<AppRoutes />);
 }
 
 export default App;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+
+const INITIAL_FORM_LOGIN = {
+  email: '',
+  password: '',
+};
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
+type FormErrors = Partial<LoginForm>;
+
+function Login() {
+  const [formData, setFormData] = useState<LoginForm>(INITIAL_FORM_LOGIN);
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  console.log(formErrors);
+
+  useEffect(() => {
+    const formValidate = (data: LoginForm) => {
+      const errors: FormErrors = {};
+      const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+      if (data.password.length < 7) {
+        errors.password = 'A senha deve ter no mínimo 6 caracteres';
+      }
+
+      if (!regex.test(formData.email)) {
+        errors.email = 'O formato do email é inválido';
+      }
+
+      setFormErrors(errors);
+      setIsFormValid(Object.keys(errors).length === 0);
+    };
+    formValidate(formData);
+  }, [formData]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setFormData(INITIAL_FORM_LOGIN);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={ handleSubmit }>
+        <input
+          type="text"
+          placeholder="Email"
+          data-testid="email-input"
+          name="email"
+          value={ formData.email }
+          onChange={ handleChange }
+        />
+        {formErrors.email && <p>{formErrors.email}</p>}
+        <input
+          type="password"
+          placeholder="Password"
+          data-testid="password-input"
+          name="password"
+          value={ formData.password }
+          onChange={ handleChange }
+        />
+        {formErrors.password && <p>{formErrors.password}</p>}
+        <button data-testid="login-submit-btn" disabled={ !isFormValid }>Enter</button>
+      </form>
+    </div>
+
+  );
+}
+
+export { Login };
