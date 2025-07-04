811c5395fc47569e5b0600dcbd001e993747d14a
Matheus Pozett <maa_souza11@hotmail.com>
2025-07-04 10:59:24 -0300

    feat: cria rotas da aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
-import { Login } from '../pages';
+import { DrinkDetail, Drinks, DrinksProgress, Favorites, Login, MealDetail, Meals,
+  MealsProgress, Profile, Recipes } from '../pages';
+import Layout from '../components/layout';
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route element={ <Layout /> }>
+        <Route path="/meals" element={ <Meals /> } />
+        <Route path="/drinks" element={ <Drinks /> } />
+        <Route path="/done-recipes" element={ <Recipes /> } />
+        <Route path="/favorite-recipes" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+      </Route>
+      <Route path="/meals/:id" element={ <MealDetail /> } />
+      <Route path="/drinks/:id" element={ <DrinkDetail /> } />
+      <Route path="/meals/:id/in-progress" element={ <MealsProgress /> } />
+      <Route path="/drinks/:id/in-progress" element={ <DrinksProgress /> } />
     </Routes>
   );
 }
