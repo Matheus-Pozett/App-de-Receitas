04de0b18f1d917b24f152b531fba635edc8c4376
Matheus Pozett <maa_souza11@hotmail.com>
2025-07-04 11:13:25 -0300

    feat: implementa logica para mostrar/esconder input de pesquisa na tela

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import imgPage from '../../images/iconeRecipesapp.svg';
 import imgProfile from '../../images/profileIcon.svg';
 import imgSearch from '../../images/searchIcon.svg';
@@ -8,17 +10,31 @@ type HeaderProps = {
 };
 
 function Header({ title, iconSearch }: HeaderProps) {
+  const [showInput, setShowInput] = useState(false);
+
+  const handleClick = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <header>
       <img src={ imgPage } alt="Logo da pagina" />
       <p>Recipes App</p>
-      {iconSearch && <img
-        src={ imgSearch }
-        alt="Icone de pesquisa"
-        data-testid="search-top-btn"
-      />}
-      <img src={ imgProfile } alt="Icone do Perfil" data-testid="profile-top-btn" />
+      {iconSearch && (
+        <button onClick={ handleClick }>
+          <img
+            src={ imgSearch }
+            alt="Icone de pesquisa"
+            data-testid="search-top-btn"
+          />
+        </button>
+      )}
+      <Link to="/profile">
+        <img src={ imgProfile } alt="Icone do Perfil" data-testid="profile-top-btn" />
+      </Link>
+
       <h1 data-testid="page-title">{title}</h1>
+      {showInput && <input data-testid="search-input" />}
     </header>
   );
 }
