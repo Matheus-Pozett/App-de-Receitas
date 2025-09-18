1142c3b81e66ff1e80660d738acf6f2573162fb3
Matheus Pozett <maa_souza11@hotmail.com>
2025-09-18 12:17:38 -0300

    style: css da pagina de perfil

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import doneIcon from '../../images/doneIcon.svg';
+import favoriteIcon from '../../images/favoritesIcon.svg';
+import logoutIcon from '../../images/logoutIcon.svg';
 
 function Profile() {
   const [email, setEmail] = useState('');
@@ -20,27 +23,51 @@ function Profile() {
   };
 
   return (
-    <div>
-      <p data-testid="profile-email">{email}</p>
-      <div>
-        <button
-          data-testid="profile-done-btn"
-          onClick={ () => navigate('/done-recipes') }
+    <div className="container text-center my-4">
+      <p
+        data-testid="profile-email"
+        className="fw-bold mb-4"
+      >
+        {email}
+      </p>
+
+      <div className="d-flex flex-column gap-4">
+        <div
+          className="d-flex align-items-center justify-content-start border-bottom pb-3"
         >
-          Done Recipes
-        </button>
-        <button
-          data-testid="profile-favorite-btn"
-          onClick={ () => navigate('/favorite-recipes') }
+          <img src={ doneIcon } alt="Done Recipe" className="me-3" />
+          <button
+            data-testid="profile-done-btn"
+            onClick={ () => navigate('/done-recipes') }
+            className="btn btn-link text-secondary text-decoration-none fw-semibold"
+          >
+            Done Recipes
+          </button>
+        </div>
+
+        <div
+          className="d-flex align-items-center justify-content-start border-bottom pb-3"
         >
-          Favorite Recipes
-        </button>
-        <button
-          data-testid="profile-logout-btn"
-          onClick={ handleLogout }
-        >
-          Logout
-        </button>
+          <img src={ favoriteIcon } alt="favorite" className="me-3" />
+          <button
+            data-testid="profile-favorite-btn"
+            onClick={ () => navigate('/favorite-recipes') }
+            className="btn btn-link text-secondary text-decoration-none fw-semibold"
+          >
+            Favorite Recipes
+          </button>
+        </div>
+
+        <div className="d-flex align-items-center justify-content-start">
+          <img src={ logoutIcon } alt="logout" className="me-3" />
+          <button
+            data-testid="profile-logout-btn"
+            onClick={ handleLogout }
+            className="btn btn-link text-secondary text-decoration-none fw-semibold"
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
