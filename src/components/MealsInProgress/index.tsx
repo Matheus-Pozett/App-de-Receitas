aea53b4a4274a7ecb905fcbf530a4eedde6d7562
Matheus Pozett <maa_souza11@hotmail.com>
2025-09-20 18:44:20 -0300

    feat: inicia componente de comidas em progresso

---
+++ src/components/MealsInProgress/index.tsx
@@ -0,0 +1,19 @@
+function MealsInProgress() {
+  return (
+    <div>
+      <img
+        src=""
+        alt=""
+        data-testid="recipe-photo"
+      />
+      <h1 data-testid="recipe-title">d</h1>
+      <button data-testid="share-btn">comó</button>
+      <button data-testid="favorite-btn">fav</button>
+      <p data-testid="recipe-category">te</p>
+      <p data-testid="instructions">ins</p>
+      <button data-testid="finish-recipe-btn">fi</button>
+    </div>
+  );
+}
+
+export default MealsInProgress;
