57a1d9ed4ba99590c2cc0db5e906f346999921bb
Matheus Pozett <maa_souza11@hotmail.com>
2025-10-11 10:44:39 -0300

    ajustes de imagens

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import drinkIcon from '../../images/drinkIcon.svg';
-import mealIcon from '../../images/mealIcon.svg';
+import drinkIcon from '../../images/drinksIcon.svg';
+import mealIcon from '../../images/icone-prato.svg';
 
 function Footer() {
   return (
