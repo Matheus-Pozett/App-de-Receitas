9070458b749f9efe3bae658e1720d3426d7b39b6
Matheus Pozett <maa_souza11@hotmail.com>
2025-10-18 12:13:03 -0300

    test: requisito 24

--- src/tests/RecipeDetail.test.tsx
+++ src/tests/RecipeDetail.test.tsx
@@ -1,4 +1,5 @@
 import { vi } from 'vitest/dist/index.js';
+import { screen, waitFor } from '@testing-library/react';
 import * as api from '../services/api';
 import { mealsMock } from './mocks/meals.mock';
 import { renderWithRouter } from './renderWithRouter';
@@ -8,19 +9,61 @@ import { drinksMock } from './mocks/drinks.mock';
 describe('Testes da pagina de detalhe da receita', () => {
   afterEach(() => vi.clearAllMocks());
 
-  test('Realiza uma request para a API de comidas passando o ID da receita que deve estar disponível nos parâmetros da URL', () => {
+  test('Realiza uma request para a API de comidas passando o ID da receita que deve estar disponível nos parâmetros da URL', async () => {
     vi.spyOn(api, 'fetchMealsById').mockResolvedValue(mealsMock[0]);
 
     renderWithRouter(<App />, { route: '/meals/53086' });
 
-    expect(api.fetchMealsById).toBeCalledWith('53086');
+    await waitFor(() => expect(api.fetchMealsById).toBeCalledWith('53086'));
   });
 
-  test('Realiza uma request para a API de bebidas passando o ID da receita que deve estar disponível nos parâmetros da URL', () => {
+  test('Realiza uma request para a API de bebidas passando o ID da receita que deve estar disponível nos parâmetros da URL', async () => {
     vi.spyOn(api, 'fetchDrinksById').mockResolvedValue(drinksMock[0]);
 
     renderWithRouter(<App />, { route: '/drinks/178349' });
 
-    expect(api.fetchDrinksById).toBeCalledWith('178349');
+    await waitFor(() => expect(api.fetchDrinksById).toBeCalledWith('178349'));
+  });
+
+  test('A tela de comida contem uma imagem da receita, um título, a categoria da receita, uma lista de ingredientes, um vídeo do YouTube incorporado e recomendações', async () => {
+    vi.spyOn(api, 'fetchMealsById').mockResolvedValue(mealsMock[0]);
+
+    renderWithRouter(<App />, { route: '/meals/53086' });
+
+    const imgRecipe = await screen.findByTestId('recipe-photo');
+    const titleRecipe = await screen.findByTestId('recipe-title');
+    const category = await screen.findByTestId('recipe-category');
+    const ingredientRecipe = await screen.findByTestId('0-ingredient-name-and-measure');
+    const instructions = await screen.findByTestId('instructions');
+    const videoRecipe = await screen.findByTestId('video');
+    const recommendations = await screen.findByTestId('0-recommendation-card');
+
+    expect(titleRecipe).toHaveTextContent(/migas/i);
+    expect(imgRecipe).toBeInTheDocument();
+    expect(category).toHaveTextContent(/Miscellaneous/i);
+    expect(ingredientRecipe).toHaveTextContent(/bread/i);
+    expect(instructions).toBeInTheDocument();
+    expect(videoRecipe).toBeInTheDocument();
+    expect(recommendations).toBeInTheDocument();
+  });
+
+  test('A tela de bebida contem uma imagem da receita, um título, se é ou não alcoólica, uma lista de ingredientes e recomendações', async () => {
+    vi.spyOn(api, 'fetchDrinksById').mockResolvedValue(drinksMock[0]);
+
+    renderWithRouter(<App />, { route: '/drinks/178349' });
+
+    const imgRecipe = await screen.findByTestId('recipe-photo');
+    const titleRecipe = await screen.findByTestId('recipe-title');
+    const category = await screen.findByTestId('recipe-category');
+    const ingredientRecipe = await screen.findByTestId('0-ingredient-name-and-measure');
+    const instructions = await screen.findByTestId('instructions');
+    const recommendations = await screen.findByTestId('0-recommendation-card');
+
+    expect(titleRecipe).toHaveTextContent(/Snowday/i);
+    expect(imgRecipe).toBeInTheDocument();
+    expect(category).toHaveTextContent(/Alcoholic/i);
+    expect(ingredientRecipe).toHaveTextContent(/Vodka/i);
+    expect(instructions).toBeInTheDocument();
+    expect(recommendations).toBeInTheDocument();
   });
 });
